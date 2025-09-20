c34823a3ddd245347dee09150faaf2c8eb122e4c
Kusk24 <winyumg2003a@gmail.com>
2025-09-20 15:18:07 +0700

    path changes added

--- app/page.js
+++ app/page.js
@@ -6,17 +6,19 @@ import Link from "next/link";
 export default function BoxBasic() {
   return (
     <main>
-      <Box component="section" className="border border-gray-800 m-5 text-center">
+      <Box
+        component="section"
+        className="border border-gray-800 m-5 text-center"
+      >
         <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
         <ul>
           <li>
-            <Link href="product">Products</Link>
+            <Link href="./product">Category</Link>
           </li>
           <li>
-            <Link href="category">Category</Link>
+            <Link href="./category">Category</Link>
           </li>
         </ul>
-        
       </Box>
     </main>
   );
