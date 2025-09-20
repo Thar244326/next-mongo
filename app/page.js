671064c6cdd152017e3909bf9a550f39fdde7487
Kusk24 <winyumg2003a@gmail.com>
2025-09-20 15:22:42 +0700

    changes added

--- app/page.js
+++ app/page.js
@@ -13,10 +13,10 @@ export default function BoxBasic() {
         <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
         <ul>
           <li>
-            <Link href="./product">Category</Link>
+            <Link href="stock/product">Product</Link>
           </li>
           <li>
-            <Link href="./category">Category</Link>
+            <Link href="stock/category">Category</Link>
           </li>
         </ul>
       </Box>
